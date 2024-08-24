3ca092196e4087fe1eab42c0c64c3af7e3105987
adesa <adesanyaenoch@gmail.com>
2024-08-24 21:14:47 +0100

    Initial Commit

---
+++ Wallpaper Explorer/Wallpaper_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallpaper_ExplorerApp.swift
+//  Wallpaper Explorer
+//
+//  Created by Enoch Adesanya on 24/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Wallpaper_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
