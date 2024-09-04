4cc3a1c5b7488815eaa4f4def205c7e41bf1c7e3
adesa <adesanyaenoch@gmail.com>
2024-09-04 13:21:56 +0100

    Add wallpaper details

---
+++ Wallpaper Explorer/UI/ExpandedWallpaperView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpandedWallpaperView.swift
+//  Wallpaper Explorer
+//
+//  Created by Enoch Adesanya on 04/09/2024.
+//
+
+import SwiftUI
+
+struct ExpandedWallpaperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpandedWallpaperView()
+}
---
+++ Wallpaper Explorer/Util/BytesToMib.swift
@@ -0,0 +1,8 @@
+//
+//  BytesToMib.swift
+//  Wallpaper Explorer
+//
+//  Created by Enoch Adesanya on 04/09/2024.
+//
+
+import Foundation
