812d236268e68127320f9c4de401ada67d2549e1
adesa <adesanyaenoch@gmail.com>
2025-01-25 12:32:44 +0100

    feature: implement wallpaper details for mobile

---
+++ Shared/PropertyLink.swift
@@ -0,0 +1,18 @@
+//
+//  PropertyLink.swift
+//  Wallpaper Explorer
+//
+//  Created by Enoch Adesanya on 25/01/2025.
+//
+
+import SwiftUI
+
+struct PropertyLink: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PropertyLink()
+}
---
+++ Shared/PropertyView.swift
@@ -0,0 +1,18 @@
+//
+//  PropertyView.swift
+//  Wallpaper Explorer
+//
+//  Created by Enoch Adesanya on 25/01/2025.
+//
+
+import SwiftUI
+
+struct PropertyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PropertyView()
+}
